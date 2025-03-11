fe2ecdd168fd266340635f88b460493cdf096e9f
Faculty <eliasbhuiyan21@gmail.com>
2025-03-11 17:07:13 +0600

    responsive nav

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import { FaBox, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router";
 import { FaChevronDown } from "react-icons/fa";
-import { IoCall } from "react-icons/io5";
+import { IoCall, IoCloseCircleSharp } from "react-icons/io5";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <header>
+    <header className="relative">
       {/* Header Top Part */}
       <div className="container">
-        <div className="flex justify-between items-center py-8">
-          <Link to="/">
-            <img src="/logo.png" alt="logo" />
+        <div className="flex justify-between items-center py-8 gap-8">
+          <button
+            onClick={() => setSidebar(true)}
+            className="md:hidden text-2xl text-primary cursor-pointer"
+          >
+            <FaBarsStaggered />
+          </button>
+          <Link to="/" className="w-32 lg:w-auto">
+            <img src="/logo.png" alt="logo" className="w-full" />
           </Link>
-          <div className="border-2 border-brand p-5 rounded w-full max-w-[700px] flex items-center">
-            <select name="" id="" className="pr-3.5 border-r-2 text-primary">
+          <div className="border-2 border-brand p-2 lg:p-5 rounded w-full max-w-64  lg:max-w-[700px] hidden md:flex items-center">
+            <select
+              name=""
+              id=""
+              className="pr-3.5 border-r-2 text-primary hidden lg:block"
+            >
               <option value="">All Categories</option>
               <option value="">All Categories</option>
               <option value="">All Categories</option>
@@ -23,21 +35,22 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Search for items..."
-              className="px-3.5 w-2/3"
+              className="px-3.5 w-full lg:w-2/3 outline-0"
             />
             <FaSearch className="ml-auto" />
           </div>
           <div>
-            <ul className="flex gap-8">
+            <ul className="flex gap-4 lg:gap-8 text-sm">
               <li>
                 <Link
                   to="/"
                   className="flex gap-2 items-end relative text-secondary"
                 >
-                  <span className="w-6 h-6 rounded-full bg-brand text-white flex items-center justify-center absolute -top-2 right-5">
+                  <span className="w-4 h-4 lg:w-6 lg:h-6 rounded-full bg-brand text-white text-xs md:text-sm flex items-center justify-center absolute -top-3 md:-top-2 -right-2 md:right-5">
                     0
                   </span>
-                  <FaShoppingCart className="text-3xl text-primary" /> Cart
+                  <FaShoppingCart className="text-2xl lg:text-3xl text-primary" />{" "}
+                  <span className="hidden md:block">Cart</span>
                 </Link>
               </li>
               <li>
@@ -45,10 +58,11 @@ const Navbar = () => {
                   to="/"
                   className="flex gap-2 items-end relative text-secondary"
                 >
-                  <span className="w-6 h-6 rounded-full bg-brand text-white flex items-center justify-center absolute -top-2 right-5">
+                  <span className="w-4 h-4 lg:w-6 lg:h-6 rounded-full bg-brand text-white text-xs md:text-sm flex items-center justify-center absolute -top-3 md:-top-2 -right-2 md:right-5">
                     0
                   </span>
-                  <FaShoppingCart className="text-3xl text-primary" /> Cart
+                  <FaShoppingCart className="text-2xl lg:text-3xl text-primary" />{" "}
+                  <span className="hidden md:block">Cart</span>
                 </Link>
               </li>
               <li>
@@ -56,27 +70,110 @@ const Navbar = () => {
                   to="/"
                   className="flex gap-2 items-end relative text-secondary"
                 >
-                  <span className="w-6 h-6 rounded-full bg-brand text-white flex items-center justify-center absolute -top-2 right-5">
+                  <span className="w-4 h-4 lg:w-6 lg:h-6 rounded-full bg-brand text-white text-xs md:text-sm flex items-center justify-center absolute -top-3 md:-top-2 -right-2 md:right-5">
                     0
                   </span>
-                  <FaShoppingCart className="text-3xl text-primary" /> Cart
+                  <FaShoppingCart className="text-2xl lg:text-3xl text-primary" />{" "}
+                  <span className="hidden md:block">Cart</span>
                 </Link>
               </li>
             </ul>
           </div>
+          {/* Main menu mobile view */}
+          <nav
+            className={`w-full h-screen bg-[rgba(0,0,0,0.6)] absolute top-0 left-0 transition-all ${
+              sidebar ? "translate-x-0" : "-translate-x-full"
+            } `}
+          >
+            <div className="w-[95%] h-full bg-white overflow-y-auto">
+              <div className="flex justify-between items-center border-b border-[#ececec] pb-4 mb-4 px-4">
+                <Link to="/" className="w-32 inline-block">
+                  <img src="/logo.png" alt="logo" />
+                </Link>
+                <button
+                  onClick={() => setSidebar(false)}
+                  className="text-2xl text-brand cursor-pointer"
+                >
+                  <IoCloseCircleSharp />
+                </button>
+              </div>
+              <div className="px-4 flex flex-col gap-4">
+                <div className="border-2 border-brand p-2 lg:p-5 rounded w-full flex items-center">
+                  <input
+                    type="text"
+                    placeholder="Search for items..."
+                    className="px-3.5 w-full lg:w-2/3 outline-0"
+                  />
+                  <FaSearch className="ml-auto" />
+                </div>
+                <ul className="font-bold text-primary text-base">
+                  <li className="w-full">
+                    <Link
+                      to="/"
+                      className="block border-b border-[#ececec] py-2"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="w-full">
+                    <Link
+                      to="/"
+                      className="block border-b border-[#ececec] py-2"
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li className="w-full">
+                    <Link
+                      to="/"
+                      className="block border-b border-[#ececec] py-2"
+                    >
+                      Shop
+                    </Link>
+                  </li>
+                  <li className="w-full">
+                    <Link
+                      to="/"
+                      className="block border-b border-[#ececec] py-2"
+                    >
+                      Mega menu
+                    </Link>
+                  </li>
+                  <li className="w-full">
+                    <Link
+                      to="/"
+                      className="block border-b border-[#ececec] py-2"
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li className="w-full">
+                    <Link
+                      to="/"
+                      className="block border-b border-[#ececec] py-2"
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </div>
       </div>
-      <nav>
-        <div className="container flex items-center gap-9">
+      {/* Main Menu desktop view*/}
+      <nav className="hidden md:block">
+        <div className="container flex items-center gap-4 xl:gap-9">
           <div className="relative">
             <h3
               onClick={() => setShow(!show)}
-              className="py-3 px-6 bg-brand rounded-md text-white font-bold flex items-center gap-2 cursor-pointer"
+              className="p-2 xl:py-3 xl:px-6 bg-brand rounded-md text-white text-xs xl:text-base font-bold flex items-center gap-1 xl:gap-2 cursor-pointer"
             >
-              <FaBox /> Browse All Categories <FaChevronDown />
+              <FaBox /> <span className="hidden xl:inline">Browse All</span>{" "}
+              Categories <FaChevronDown />
             </h3>
             {show && (
-              <div className="w-md rounded-lg absolute top-20 left-0 p-7 border-2 border-brand grid grid-cols-2 gap-6">
+              <div className="bg-white w-md rounded-lg absolute top-10 xl:top-20 left-0 p-7 border-2 border-brand grid grid-cols-2 gap-6">
                 <div className="flex items-center gap-2 cursor-pointer shadow py-2 px-3">
                   <img src="/category-1.svg" alt="category1" className="w-6" />
                   <p className="font-bold text-sm text-primary">
@@ -116,7 +213,7 @@ const Navbar = () => {
               </div>
             )}
           </div>
-          <ul className="flex gap-8 font-bold text-primary text-lg">
+          <ul className="flex gap-3 xl:gap-6 font-bold text-primary text-base xl:text-lg">
             <li>
               <Link to="/" className="hover:text-brand transition">
                 Home
@@ -152,10 +249,13 @@ const Navbar = () => {
             to="tel: 1900 - 888"
             className="ml-auto flex items-center gap-2"
           >
-            <IoCall className="text-4xl" />
+            <IoCall className="text-2xl xl:text-4xl" />
             <div>
-              <p className="text-brand font-bold text-2xl"> 1900 - 888</p>
-              <p className="text-secondary font-medium text-sm">
+              <p className="text-brand font-bold text-base xl:text-2xl">
+                {" "}
+                1900 - 888
+              </p>
+              <p className="text-secondary font-medium text-xs xl:text-sm">
                 24/7 Support Center
               </p>
             </div>
